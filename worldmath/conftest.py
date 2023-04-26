75002a05d71edb621679e6d41ce86d91e6d1648d
Raphael Muniz <raphael.muniz@ifrn.edu.br>
2023-04-26 13:33:49 -0300

    Commit inicial. Build project cookiecutter.

---
+++ worldmath/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from worldmath.users.models import User
+from worldmath.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
